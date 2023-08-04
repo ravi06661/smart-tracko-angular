ff4858ebf160f83e62417ca391c57636cfeee790
dollop.ravi <dollop.ravi@gmail.com>
2023-08-04 18:25:35 +0530

    myc

---
+++ src/app/admin/admin-student-registration/admin-student-registration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-student-registration',
+  templateUrl: './admin-student-registration.component.html',
+  styleUrls: ['./admin-student-registration.component.scss']
+})
+export class AdminStudentRegistrationComponent {
+
+}
