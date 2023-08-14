b8b35cb3aff3a4ee0aa5c555bd013cb48b7a9f44
dollop.ravi <dollop.ravi@gmail.com>
2023-08-14 17:06:27 +0530

    second

--- src/app/admin/admin-add-fees/admin-add-fees.component.ts
+++ src/app/admin/admin-add-fees/admin-add-fees.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StudentDetails } from 'src/app/entity/student-details';
+import { StudentService } from 'src/app/service/student.service';
 
 @Component({
   selector: 'app-admin-add-fees',
   templateUrl: './admin-add-fees.component.html',
   styleUrls: ['./admin-add-fees.component.scss']
 })
-export class AdminAddFeesComponent {
+export class AdminAddFeesComponent implements OnInit{
 
+  student:StudentDetails[]=[]
+  constructor(private studentService:StudentService){}
+  ngOnInit(): void {
+    
+    this.allStudent();
+  }
+
+  public allStudent(){
+    this.studentService.allStudent().subscribe({
+      next:(data:any)=>{
+        this.student=data
+      }
+    })
+  }
 }
