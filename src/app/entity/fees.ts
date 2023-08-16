50a4674a6eb57118d48682e20b56026430ac247b
dollop.ravi <dollop.ravi@gmail.com>
2023-08-16 15:11:55 +0530

    wednesday

--- src/app/entity/fees.ts
+++ src/app/entity/fees.ts
@@ -1,4 +1,17 @@
+import { Course } from "./course";
+import { StudentDetails } from "./student-details";
+
 export class Fees {
 
+    public 	 feesId:number=0;
+    public studentdetalis:StudentDetails=new StudentDetails;
+    public  finalFees:number=0;
+	public  feesPay:number=0;
+    public course:Course=new Course;
+	public  remainingFees:number=0;
+    public  date:Date | undefined;
+	public  createdDate:Date | undefined;
+	public  updatedDate:Date | undefined;
+	public  isCompleted:boolean=false;
     
 }
