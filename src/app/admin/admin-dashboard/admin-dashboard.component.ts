9ae5d41e4f293c36e344b3a160730744773b9c56
kapilrathore357 <rathorekapil537@gmail.com>
2023-08-19 18:27:23 +0530

    kapil

--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -153,3 +153,4 @@ export class AdminDashboardComponent implements OnInit {
     return years;
   }
 }
+
--- src/app/admin/admin-subjects-chapter-quiz/admin-subjects-chapter-quiz.component.ts
+++ src/app/admin/admin-subjects-chapter-quiz/admin-subjects-chapter-quiz.component.ts
@@ -4,7 +4,7 @@ import { isThisHour } from 'date-fns';
 import { lastDayOfDecade } from 'date-fns/esm';
 import { ChapterQuizeQuestion } from 'src/app/entity/chapter-quize-question';
 import { QuestionServiceService } from 'src/app/service/question-service.service';
-
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 @Component({
   selector: 'app-admin-subjects-chapter-quiz',
   templateUrl: './admin-subjects-chapter-quiz.component.html',
@@ -15,6 +15,10 @@ export class AdminSubjectsChapterQuizComponent {
   questions: ChapterQuizeQuestion[] = []
   id: number = 0;
   question: ChapterQuizeQuestion = new ChapterQuizeQuestion();
+  public Editor = ClassicEditor;
+  static images: File[] = []
+  private editorInstance: any;
+  message:string=''
   constructor(private questionService: QuestionServiceService, private route: ActivatedRoute, private router: Router) {
 
   }
@@ -22,6 +26,16 @@ export class AdminSubjectsChapterQuizComponent {
     this.id = this.route.snapshot.params[('id')];
     this.getAllQuestions();
   }
+
+   public addQuestion(){  
+     this.questionService.addQuestion(this.question,this.id).subscribe(
+      (data)=>{
+        this.question = new ChapterQuizeQuestion();
+        this.message='success..'
+        console.log(data);
+      }
+     )
+   }
   public getAllQuestions() {
     if (this.id) {
       this.questionService.getAllQuestionByChapterId(this.id).subscribe(
@@ -36,15 +50,23 @@ export class AdminSubjectsChapterQuizComponent {
       (data) => {
         this.questionId = 0;
         this.ngOnInit();
-      }, (error) => {
-        console.log('errore')
+      },(error)=>{
+        this.questionId = 0;
+        this.ngOnInit();
       }
     )
   }
   public updateQuestion() {
-    this.questionService.updateQuestionById(this.question).subscribe(
+    const textElement = document.createElement('div');
+    textElement.innerHTML = this.editorInstance.getData();
+    const text = textElement.innerText.trim();
+   this.question.questionContent=text;
+    this.questionService.updateQuestionById(this.question,this.id).subscribe(
       (data) => {
-        alert('success')
+      this.question =new ChapterQuizeQuestion();
+      this.message ="success.."
+      },(error)=>{
+      
       }
     )
   }
@@ -53,13 +75,24 @@ export class AdminSubjectsChapterQuizComponent {
     this.questionService.getQuestionById(id).subscribe(
       (data) => {
         this.question = data;
-        console.log(data);
-        //  this.updateQuestion()
       }
     )
   }
 
-  public Cancel() {
+  public cancel() {
     this.question = new ChapterQuizeQuestion();
+  }
+  reload(){
+    this.question = new ChapterQuizeQuestion();
+    this.getAllQuestions();
+    this.message=''
+  }
+  onEditorReady(eventData: any) {
+    this.editorInstance = eventData;
+  }
+  ckSubmit() {
+    const textElement = document.createElement('div');
+    textElement.innerHTML = this.editorInstance.getData();
+    const text = textElement.innerText.trim();
   }
 }
