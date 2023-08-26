6b7f718c40a1a1908e4047f24545dc9d9f304095
rahulh.dollop <rahulh.dollop@gmail.com>
2023-08-26 10:57:40 +0530

    rahul hencha

--- src/app/payload/assignment-request.ts
+++ src/app/payload/assignment-request.ts
@@ -2,8 +2,8 @@ import { Assignment } from 'src/app/entity/assignment';
 import { TaskQuestion } from '../entity/task-question';
 export class AssignmentRequest {
     public title:string = '';
-    public taskAttachment:string = '';
-    public assginmentQuestion:TaskQuestion[]=[];
+    // public taskAttachment:string = '';
+    // public assginmentQuestion:TaskQuestion[]=[];
     public courseId:number = 0;
     public subjectId:number = 0;
 }
---
+++ src/app/payload/task-question-request.spec.ts
@@ -0,0 +1,7 @@
+import { TaskQuestionRequest } from './task-question-request';
+
+describe('TaskQuestionRequest', () => {
+  it('should create an instance', () => {
+    expect(new TaskQuestionRequest()).toBeTruthy();
+  });
+});
