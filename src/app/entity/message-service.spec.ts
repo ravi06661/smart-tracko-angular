fe24d0531716d53c4c92afb1d0bc989be8b4ae58
kapilrathore357 <rathorekapil537@gmail.com>
2023-10-19 18:17:48 +0530

    angularkapil

---
+++ src/app/entity/message-service.spec.ts
@@ -0,0 +1,7 @@
+import { MessageService } from './message-service';
+
+describe('MessageService', () => {
+  it('should create an instance', () => {
+    expect(new MessageService()).toBeTruthy();
+  });
+});
