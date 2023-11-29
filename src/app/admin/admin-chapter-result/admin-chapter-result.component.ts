1fd91ca88572bdad7b0462249f8a86b9a71db0f3
dollop.ravi <dollop.ravi@gmail.com>
2023-11-29 17:49:00 +0530

    first

--- src/app/admin/admin-chapter-result/admin-chapter-result.component.ts
+++ src/app/admin/admin-chapter-result/admin-chapter-result.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { log } from 'console';
+import { ChapterExamResult } from 'src/app/entity/chapter-exam-result';
+import { ChatServiceService } from 'src/app/service/chat-service-service.service';
+import { ExamServiceService } from 'src/app/service/exam-service.service';
+import { UtilityServiceService } from 'src/app/service/utility-service.service';
 
 @Component({
   selector: 'app-admin-chapter-result',
   templateUrl: './admin-chapter-result.component.html',
   styleUrls: ['./admin-chapter-result.component.scss']
 })
-export class AdminChapterResultComponent {
+export class AdminChapterResultComponent implements OnInit{
+     
+  chapterExamResult:ChapterExamResult[]=[];
+  chapterId:number=0;
 
+  BASE_URL = this.utilityService.getBaseUrl();
+  imageUrl = this.BASE_URL + '/file/getImageApi/images/';
+  constructor(private examService:ExamServiceService,private activateRoute:ActivatedRoute,private utilityService: UtilityServiceService){}
+  ngOnInit(): void {
+    this.chapterId = this.activateRoute.snapshot.params[('chapterId')];
+    this.showResult();
+  }
+
+  showResult(){
+
+    this.examService.getAllChapterExamResultByChaterId(this.chapterId).subscribe((data:any)=>{
+      console.log(data);
+      
+        this.chapterExamResult=data.examResult
+
+    })
+         
+  }
 }
